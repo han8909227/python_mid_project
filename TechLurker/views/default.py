ba6df08b0514eed542558e763f133f34704a9090
maxawolff <maxawolff@hotmail.com>
2017-11-16 11:24:28 -0800

    working on views and graphs

--- TechLurker/views/default.py
+++ TechLurker/views/default.py
@@ -39,13 +39,12 @@ class LurkerViews:
                 loc_list.append(data.loc)
                 job_list = parse(data.job_type)
                 job_types = job_types + job_list
-            # text = text.lower()
-            # word_count = cw(text)
             dict = gt.get_job_locations_from_db(loc_list)
-            tag1 = gt.dict_to_pie_chart_tag(dict)
+            tag1 = gt.dict_to_pie_chart_tag(dict, "Programming Jobs by Country")
             job_dict = gt.get_job_types_from_db(job_types)
-            tag2 = gt.dict_to_pie_chart_tag(job_dict)
-            return {'tag': tag1, 'tag2': tag2}
+            tag2 = gt.dict_to_pie_chart_tag(job_dict, "Programming Job Types")
+            return {'tag': tag1, 'tag2': tag2, 'result': 'job'}
+
         elif selected == 'programming_languages':
             raw_data = self.request.dbsession.query(RedditData).all()
             text = ''
@@ -53,8 +52,10 @@ class LurkerViews:
                 text = text + ' ' + data.content
             text = text.lower()
             word_count = cw(text)
-            tag = gt.generate_chart_on_keyword_v2(gt.languages, word_count)
-            return {'tag': tag}
+            tag = gt.generate_chart_on_keyword_v2(gt.languages, word_count, 'Language Popularity')
+            tag2 = gt.generate_pie_chart_on_keyword(gt.languages, word_count, "Language Popularity")
+            return {'tag': tag, 'tag2': tag2, 'result': 'language'}
+
         elif selected == 'security':
             raw_data = self.request.dbsession.query(SecurityNewsData).all()
             text = ''
@@ -62,8 +63,10 @@ class LurkerViews:
                 text = text + ' ' + data.articleContent
             text = text.lower()
             word_count = cw(text)
-            tag = gt.generate_chart_on_keyword_v2(gt.security, word_count)
-            return {'tag': tag}
+            tag = gt.generate_chart_on_keyword_v2(gt.security, word_count, 'security')
+            tag2 = gt.generate_pie_chart_on_keyword(gt.security, word_count, "security")
+            return {'tag': tag, 'tag2': tag2, 'result': 'security'}
+
         elif selected == 'programming_questions':
             raw_data = self.request.dbsession.query(RedditData).all()
             top = gt.TopFive()
@@ -74,9 +77,11 @@ class LurkerViews:
             text = text.lower()
             word_count = cw(text)
             search_terms = ['algorithm', 'sequence', 'memory', 'search', 'efficient', 'functions', 'generate', 'syntax', 'optimize']
-            tag = gt.generate_chart_on_keyword_v2(search_terms, word_count)
+            tag = gt.generate_chart_on_keyword_v2(search_terms, word_count, 'Most asked programming questions')
+            tag2 = gt.generate_pie_chart_on_keyword(search_terms, word_count, "Most asked programming questions")
             del top.container[0]
-            return {'tag': tag, 'top': top.container}
+            return {'tag': tag, 'tag2': tag2, 'top': top.container, 'result': 'questions'}
+
         elif selected == 'webdev':
             raw_data = self.request.dbsession.query(TechRepublicData).all()
             web_data = ''
