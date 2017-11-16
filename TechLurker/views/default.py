35cee0a8cb4b1fe1a0306f69439a945fd7137648
maxawolff <maxawolff@hotmail.com>
2017-11-15 17:24:08 -0800

    eddited css, looking and wordcounts for other scrapes

--- TechLurker/views/default.py
+++ TechLurker/views/default.py
@@ -5,7 +5,7 @@ from pyramid.view import view_config, view_defaults
 import graph as gt
 import pdb
 from pyramid.httpexceptions import HTTPNotFound, HTTPFound
-from TechLurker.models.mymodel import RedditData, SecurityNewsData, PyjobData
+from TechLurker.models.mymodel import RedditData, SecurityNewsData, PyjobData, TechRepublicData
 from TechLurker.searching import count_words as cw
 from TechLurker.searching import parse_job_titles as parse
 
@@ -32,24 +32,20 @@ class LurkerViews:
         url_list = self.request.url.split('/')
         selected = url_list[-1]
         if selected == 'job_posts':
-            words = ""
-            raw_data = self.request.dbsession.query(RedditData).all()
+            raw_data = self.request.dbsession.query(PyjobData).all()
+            loc_list = []
+            job_types = []
             for data in raw_data:
-                words = words + data.content
-            pdb.set_trace()
-            # loc_list = []
-            # job_types = []
-            # for data in raw_data:
-            #     loc_list.append(data.loc)
-            #     job_list = parse(data.job_type)
-            #     job_types = job_types + job_list
-            # # text = text.lower()
-            # # word_count = cw(text)
-            # dict = gt.get_job_locations_from_db(loc_list)
-            # tag1 = gt.dict_to_pie_chart_tag(dict)
-            # job_dict = gt.get_job_types_from_db(job_types)
-            # tag2 = gt.dict_to_pie_chart_tag(job_dict)
-            # return {'tag': tag1, 'tag2': tag2}
+                loc_list.append(data.loc)
+                job_list = parse(data.job_type)
+                job_types = job_types + job_list
+            # text = text.lower()
+            # word_count = cw(text)
+            dict = gt.get_job_locations_from_db(loc_list)
+            tag1 = gt.dict_to_pie_chart_tag(dict)
+            job_dict = gt.get_job_types_from_db(job_types)
+            tag2 = gt.dict_to_pie_chart_tag(job_dict)
+            return {'tag': tag1, 'tag2': tag2}
         elif selected == 'programming_languages':
             raw_data = self.request.dbsession.query(RedditData).all()
             text = ''
@@ -75,13 +71,20 @@ class LurkerViews:
             for data in raw_data:
                 text = text + ' ' + data.content
                 top.add_new_post(data)
-            pdb.set_trace()
             text = text.lower()
             word_count = cw(text)
             search_terms = ['algorithm', 'sequence', 'memory', 'search', 'efficient', 'functions', 'generate', 'syntax', 'optimize']
             tag = gt.generate_chart_on_keyword_v2(search_terms, word_count)
             del top.container[0]
             return {'tag': tag, 'top': top.container}
+        elif selected == 'webdev':
+            raw_data = self.request.dbsession.query(TechRepublicData).all()
+            web_data = ''
+            for post in raw_data:
+                if post.from_forum == "web_development":
+                    web_data = web_data + post.content + ' '
+            word_count = cw(web_data)
+            pdb.set_trace()
         return {}
 
     @view_config(route_name='about', renderer='../templates/about.jinja2')
