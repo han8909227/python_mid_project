f90d527eeedb31b8fceccd52d7cebff4539f8453
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-11-16 12:52:57 -0500

    Wrote tests for successfuly aquiring job locations, types, and generating a pie chart.

--- TechLurker/tests.py
+++ TechLurker/tests.py
@@ -286,3 +286,45 @@ def test_chart_on_keyword_returns_type_error_if_no_data():
         generate_chart_on_keyword_v2(count_words)
 
 
+def test_get_job_locations_from_db(dummy_request):
+    """Test that job locations are collected from data."""
+    from graph import get_job_locations_from_db
+    loc_list = ['USA']
+    new_lock = PyjobData(
+        title='test_title',
+        descrip='test description',
+        loc='Seattle, WA, USA',
+        job_type='backend',
+        url='randome.com'
+    )
+    dummy_request.dbsession.add(new_lock)
+    dummy_request.dbsession.commit()
+    assert 'usa' in get_job_locations_from_db(loc_list)
+
+
+def test_get_job_type_from_db(dummy_request):
+    """Test that job locations are collected from data."""
+    from graph import get_job_types_from_db
+    job_types = ['backend', 'AI']
+    new_lock = PyjobData(
+        title='test_title',
+        descrip='test description',
+        loc='Seattle, WA, USA',
+        job_type='backend',
+        url='randome.com'
+    )
+    dummy_request.dbsession.add(new_lock)
+    dummy_request.dbsession.commit()
+    assert 'backend' in get_job_types_from_db(job_types)
+
+
+def test_dict_to_pie_chart_url():
+    """Test that url with div tag is returned."""
+    from graph import dict_to_pie_chart_tag
+    mock_dict = {
+        '1': 1,
+        '2': 2,
+        '3': 3
+    }
+    result = dict_to_pie_chart_tag(mock_dict)
+    assert '<div>' in result
